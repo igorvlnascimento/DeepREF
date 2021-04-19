07a319eeb5d40ed151005a9e7c9062a864166704
Igor Nascimento <igorvlnascimento@gmail.com>
2021-04-19 20:57:48 -0300

    added new bert model

--- opennre/encoder/__init__.py
+++ opennre/encoder/__init__.py
@@ -5,6 +5,7 @@ from __future__ import print_function
 from .cnn_encoder import CNNEncoder
 from .pcnn_encoder import PCNNEncoder
 from .bert_encoder import BERTEncoder, BERTEntityEncoder
+from .bert_cnn_entity_encoder import BERTCNNEntityEncoder
 from .roberta_encoder import RoBERTaEncoder, RoBERTaEntityEncoder
 from .distilbert_encoder import DistilBertEncoder, DistilBertEntityEncoder
 from .gpt2_encoder import GPT2Encoder, GPT2EntityEncoder
@@ -14,6 +15,7 @@ __all__ = [
     'PCNNEncoder',
     'BERTEncoder',
     'BERTEntityEncoder',
+    'BERTCNNEntityEncoder',
     'RoBERTaEntityEnconder',
     'RoBERTaEnconder',
     'DistilBertEntityEnconder',
