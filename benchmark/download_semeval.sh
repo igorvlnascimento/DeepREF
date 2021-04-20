3928553718b5bd74e0a2dd0a6a7da730ca919619
Igor Nascimento <igorvlnascimento@gmail.com>
2021-04-19 21:37:34 -0300

    little changes

--- benchmark/download_semeval.sh
+++ benchmark/download_semeval.sh
@@ -1,5 +1,5 @@
-mkdir semeval
-wget -P semeval https://thunlp.oss-cn-qingdao.aliyuncs.com/opennre/benchmark/semeval/semeval_rel2id.json
-wget -P semeval https://thunlp.oss-cn-qingdao.aliyuncs.com/opennre/benchmark/semeval/semeval_train.txt
-wget -P semeval https://thunlp.oss-cn-qingdao.aliyuncs.com/opennre/benchmark/semeval/semeval_val.txt
-wget -P semeval https://thunlp.oss-cn-qingdao.aliyuncs.com/opennre/benchmark/semeval/semeval_test.txt
+mkdir benchmark/semeval
+wget -P benchmark/semeval https://thunlp.oss-cn-qingdao.aliyuncs.com/opennre/benchmark/semeval/semeval_rel2id.json
+wget -P benchmark/semeval https://thunlp.oss-cn-qingdao.aliyuncs.com/opennre/benchmark/semeval/semeval_train.txt
+wget -P benchmark/semeval https://thunlp.oss-cn-qingdao.aliyuncs.com/opennre/benchmark/semeval/semeval_val.txt
+wget -P benchmark/semeval https://thunlp.oss-cn-qingdao.aliyuncs.com/opennre/benchmark/semeval/semeval_test.txt
--- opennre/encoder/__init__.py
+++ opennre/encoder/__init__.py
@@ -6,9 +6,9 @@ from .cnn_encoder import CNNEncoder
 from .pcnn_encoder import PCNNEncoder
 from .bert_encoder import BERTEncoder, BERTEntityEncoder
 from .bert_cnn_entity_encoder import BERTCNNEntityEncoder
-from .roberta_encoder import RoBERTaEncoder, RoBERTaEntityEncoder
-from .distilbert_encoder import DistilBertEncoder, DistilBertEntityEncoder
-from .gpt2_encoder import GPT2Encoder, GPT2EntityEncoder
+#from .roberta_encoder import RoBERTaEncoder, RoBERTaEntityEncoder
+#from .distilbert_encoder import DistilBertEncoder, DistilBertEntityEncoder
+#from .gpt2_encoder import GPT2Encoder, GPT2EntityEncoder
 
 __all__ = [
     'CNNEncoder',
