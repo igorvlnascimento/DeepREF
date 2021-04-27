b88066fd59116d4b8807bb3adf7b8b9c8dbb81b0
Igor Nascimento <igorvlnascimento@gmail.com>
2021-04-26 23:43:26 -0300

    fix bert encoder2

--- opennre/encoder/bert_encoder.py
+++ opennre/encoder/bert_encoder.py
@@ -206,3 +206,94 @@ class BERTEntityEncoder(nn.Module):
         att_mask[0, :avai_len] = 1
 
         return indexed_tokens, att_mask, pos1, pos2
+
+class BERTHiddenStateEncoder(nn.Module):
+    def __init__(self, max_length, pretrain_path, blank_padding=True, mask_entity=False):
+        """
+        Args:
+            max_length: max length of sentence
+            pretrain_path: path of pretrain model
+        """
+        super().__init__()
+        self.max_length = max_length
+        self.blank_padding = blank_padding
+        self.hidden_size = 768
+        self.mask_entity = mask_entity
+        logging.info('Loading BERT pre-trained checkpoint.')
+        self.bert = BertModel.from_pretrained(pretrain_path)
+        self.tokenizer = BertTokenizer.from_pretrained(pretrain_path)
+
+    def forward(self, token, att_mask):
+        """
+        Args:
+            token: (B, L), index of tokens
+            att_mask: (B, L), attention mask (1 for contents and 0 for padding)
+        Return:
+            (B, H), representations for sentences
+        """
+        _, x, hs, atts = self.bert(token, attention_mask=att_mask)
+        return x, hs, atts
+
+    def tokenize(self, item):
+        """
+        Args:
+            item: data instance containing 'text' / 'token', 'h' and 't'
+        Return:
+            Name of the relation of the sentence
+        """
+        # Sentence -> token
+        if 'text' in item:
+            sentence = item['text']
+            is_token = False
+        else:
+            sentence = item['token']
+            is_token = True
+        pos_head = item['h']['pos']
+        pos_tail = item['t']['pos']
+
+        pos_min = pos_head
+        pos_max = pos_tail
+        if pos_head[0] > pos_tail[0]:
+            pos_min = pos_tail
+            pos_max = pos_head
+            rev = True
+        else:
+            rev = False
+        
+        if not is_token:
+            sent0 = self.tokenizer.tokenize(sentence[:pos_min[0]])
+            ent0 = self.tokenizer.tokenize(sentence[pos_min[0]:pos_min[1]])
+            sent1 = self.tokenizer.tokenize(sentence[pos_min[1]:pos_max[0]])
+            ent1 = self.tokenizer.tokenize(sentence[pos_max[0]:pos_max[1]])
+            sent2 = self.tokenizer.tokenize(sentence[pos_max[1]:])
+        else:
+            sent0 = self.tokenizer.tokenize(' '.join(sentence[:pos_min[0]]))
+            ent0 = self.tokenizer.tokenize(' '.join(sentence[pos_min[0]:pos_min[1]]))
+            sent1 = self.tokenizer.tokenize(' '.join(sentence[pos_min[1]:pos_max[0]]))
+            ent1 = self.tokenizer.tokenize(' '.join(sentence[pos_max[0]:pos_max[1]]))
+            sent2 = self.tokenizer.tokenize(' '.join(sentence[pos_max[1]:]))
+
+        if self.mask_entity:
+            ent0 = ['[unused4]'] if not rev else ['[unused5]']
+            ent1 = ['[unused5]'] if not rev else ['[unused4]']
+        else:
+            ent0 = ['[unused0]'] + ent0 + ['[unused1]'] if not rev else ['[unused2]'] + ent0 + ['[unused3]']
+            ent1 = ['[unused2]'] + ent1 + ['[unused3]'] if not rev else ['[unused0]'] + ent1 + ['[unused1]']
+
+        re_tokens = ['[CLS]'] + sent0 + ent0 + sent1 + ent1 + sent2 + ['[SEP]']
+        
+        indexed_tokens = self.tokenizer.convert_tokens_to_ids(re_tokens)
+        avai_len = len(indexed_tokens)
+
+        # Padding
+        if self.blank_padding:
+            while len(indexed_tokens) < self.max_length:
+                indexed_tokens.append(0)  # 0 is id for [PAD]
+            indexed_tokens = indexed_tokens[:self.max_length]
+        indexed_tokens = torch.tensor(indexed_tokens).long().unsqueeze(0)  # (1, L)
+
+        # Attention mask
+        att_mask = torch.zeros(indexed_tokens.size()).long()  # (1, L)
+        att_mask[0, :avai_len] = 1
+
+        return indexed_tokens, att_mask
