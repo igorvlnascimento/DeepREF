803e414e0deea6b02630e630ef5706d4d1af6331
Igor Nascimento <igorvlnascimento@gmail.com>
2021-06-18 00:35:33 -0300

    fix errors in parser2

--- example/parser.py
+++ example/parser.py
@@ -1,8 +1,8 @@
 import subprocess
-from opennre.constants import preprocessing_choices, datasets_choices, sem_eval_model_choices, ddi_model_choices
+from opennre.constants import preprocessing_choices, datasets_choices, semeval_model_choices, ddi_model_choices
 
 if __name__ == '__main__':
-    for model in sem_eval_model_choices:
+    for model in semeval_model_choices:
         for preprocessing in preprocessing_choices:
             subprocess.call(['python', 'example/train_supervised_{}.py'.format(model), '--dataset', 'semeval2010', '--preprocessing', preprocessing])
 
