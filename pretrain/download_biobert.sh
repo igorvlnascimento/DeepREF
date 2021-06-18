6fd0c5db31a1eb7196a4fa9af9f8fb320a109388
Igor Nascimento <igorvlnascimento@gmail.com>
2021-06-18 12:42:38 -0300

    refactor pretrain folder

--- pretrain/download_biobert.sh
+++ pretrain/download_biobert.sh
@@ -1,4 +1,4 @@
-mkdir pretrain/biobert-v1.1
-wget -P pretrain/biobert-v1.1 https://huggingface.co/dmis-lab/biobert-v1.1/blob/main/config.json
-wget -P pretrain/biobert-v1.1 https://huggingface.co/dmis-lab/biobert-v1.1/blob/main/pytorch_model.bin
-wget -P pretrain/biobert-v1.1 https://huggingface.co/dmis-lab/biobert-v1.1/blob/main/vocab.txt
+mkdir pretrain/biobert
+wget -P pretrain/biobert https://huggingface.co/dmis-lab/biobert-v1.1/blob/main/config.json
+wget -P pretrain/biobert https://huggingface.co/dmis-lab/biobert-v1.1/blob/main/pytorch_model.bin
+wget -P pretrain/biobert https://huggingface.co/dmis-lab/biobert-v1.1/blob/main/vocab.txt
