5f042cb4c450f7c8f5333822a3e3dee2a25ad063
Igor Nascimento <igorvlnascimento@gmail.com>
2021-06-19 09:42:40 -0300

    remove unuseful scripts

--- opennre/encoder/__init__.py
+++ opennre/encoder/__init__.py
@@ -5,11 +5,9 @@ from __future__ import print_function
 from .cnn_encoder import CNNEncoder
 from .pcnn_encoder import PCNNEncoder
 from .bert_encoder import BERTEncoder, BERTEntityEncoder
-from .redn_encoder import BERTHiddenStateEncoder
 from .bert_cnn_entity_encoder import BERTCNNEntityEncoder
 from .roberta_encoder import RoBERTaEncoder, RoBERTaEntityEncoder
 from .distilbert_encoder import DistilBertEncoder, DistilBertEntityEncoder
-#from .gpt2_encoder import GPT2Encoder, GPT2EntityEncoder
 
 __all__ = [
     'CNNEncoder',
