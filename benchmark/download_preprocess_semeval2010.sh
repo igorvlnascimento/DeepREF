65fecec04474314444f2538a1111607acd4215e6
Igor Nascimento <igorvlnascimento@gmail.com>
2021-06-19 11:10:14 -0300

    improve bash commands

--- benchmark/download_preprocess_semeval2010.sh
+++ benchmark/download_preprocess_semeval2010.sh
@@ -2,3 +2,4 @@ mkdir benchmark/raw_semeval
 wget -P benchmark/raw_semeval https://raw.githubusercontent.com/sahitya0000/Relation-Classification/master/corpus/SemEval2010_task8_training/TRAIN_FILE.TXT
 wget -P benchmark/raw_semeval https://raw.githubusercontent.com/sahitya0000/Relation-Classification/master/corpus/SemEval2010_task8_testing_keys/TEST_FILE_FULL.TXT
 python pre_processing/preprocess_semeval.py
+rm -r benchmark/raw_semeval
