7199d82f233d4254bfeecf76eabab80c6ad50b45
Igor Nascimento <igorvlnascimento@gmail.com>
2021-06-20 13:09:19 -0300

    delete gpt2 pretrain

--- pretrain/download_gpt2.sh
+++
@@ -1,4 +0,0 @@
-mkdir gpt2
-wget -P gpt2 https://huggingface.co/gpt2/resolve/main/config.json
-wget -P gpt2 https://huggingface.co/gpt2/resolve/main/pytorch_model.bin
-wget -P gpt2 https://huggingface.co/gpt2/resolve/main/vocab.json
