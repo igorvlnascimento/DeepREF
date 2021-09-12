2699305e18e179bdeb0503f511d2924f5400c26d
Igor Nascimento <igorvlnascimento@gmail.com>
2021-09-12 00:03:14 -0300

    added gru encoder

--- opennre/module/nn/__init__.py
+++ opennre/module/nn/__init__.py
@@ -4,10 +4,12 @@ from __future__ import print_function
 
 from .cnn import CNN
 from .rnn import RNN
+from .gru import GRU
 from .lstm import LSTM
 
 __all__ = [
     'CNN',
     'RNN',
+    'GRU',
     'LSTM',
 ]
