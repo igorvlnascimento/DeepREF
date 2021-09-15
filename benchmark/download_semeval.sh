729f7acdcb09a9a8e8fb2d1f6ed02d8bb9a68b13
igorvlnascimento <igorvlnascimento@gmail.com>
2021-09-15 17:53:10 -0300

    Delete download_semeval.sh

--- benchmark/download_semeval.sh
+++
@@ -1,5 +0,0 @@
-mkdir benchmark/semeval
-wget -P benchmark/semeval https://thunlp.oss-cn-qingdao.aliyuncs.com/opennre/benchmark/semeval/semeval_rel2id.json
-wget -P benchmark/semeval https://thunlp.oss-cn-qingdao.aliyuncs.com/opennre/benchmark/semeval/semeval_train.txt
-wget -P benchmark/semeval https://thunlp.oss-cn-qingdao.aliyuncs.com/opennre/benchmark/semeval/semeval_val.txt
-wget -P benchmark/semeval https://thunlp.oss-cn-qingdao.aliyuncs.com/opennre/benchmark/semeval/semeval_test.txt
