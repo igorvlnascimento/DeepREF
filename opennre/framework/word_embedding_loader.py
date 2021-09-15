ddbfaa2bacc286c2fb431aa0f7bbf161808e501a
Igor Nascimento <igorvlnascimento@gmail.com>
2021-09-15 10:00:52 -0300

    update word embedding loader

--- opennre/framework/word_embedding_loader.py
+++ opennre/framework/word_embedding_loader.py
@@ -1,5 +1,6 @@
 import torch
 import numpy as np
+from gensim.models import KeyedVectors
 
 import io
 import os
@@ -53,37 +54,27 @@ class WordEmbeddingLoader(object):
             word2vec = torch.from_numpy(word2vec)
         
         elif 'fasttext_wiki' == self.embedding_name:
-            with open(os.path.join(self.path_word, 'wiki-300d-1M.vec'), 'r', encoding='utf-8') as fr:
-                for line in fr:
-                    print(line)
-                    line = line.strip().split()
-                    if len(line) != self.word_dim + 1:
-                        continue
-                    word2id[line[0]] = len(word2id)
-                    word2vec.append(np.asarray(line[1:], dtype=np.float32))
-
-            special_emb = np.random.uniform(-1, 1, (2, self.word_dim))
-            special_emb[0] = 0  # <pad> is initialize as zero
-            word2vec = np.concatenate((special_emb, word2vec), axis=0)
-            word2vec = word2vec.astype(np.float32).reshape(-1, self.word_dim)
-            word2vec = torch.from_numpy(word2vec)
+            if not os.path.exists(os.path.join(self.path_word, 'fasttext_wiki.1M.300d_word2id.json')):
+                fasttext_model = KeyedVectors.load_word2vec_format(os.path.join(self.path_word, 'wiki-300d-1M.vec'))
+                word2id = fasttext_model.vocab
+                json.dump(word2id, open(os.path.join(self.path_word,'fasttext_wiki.1M.300d_word2id.json')))
+                word2vec = np.array([fasttext_model.word_vec(k) for k in word2id.keys()])
+                np.save(os.path.join(self.path_word,'fasttext_wiki.1M.300d._mat.npy'), word2vec)
+            else:
+                word2id = json.load(open(os.path.join(self.path_word, 'fasttext_wiki.1M.300d_word2id.json')))
+                word2vec = np.load(os.path.join(self.path_word, 'fasttext_wiki.1M.300d._mat.npy'))
+                
         
         elif 'fasttext_crawl' == self.embedding_name:
-            fr = open(os.path.join(self.path_word, 'crawl-300d-2M.vec'), 'r', encoding='utf-8').readlines()
-            for i in tqdm(range(len(fr))):
-                line = fr[i]
-                line = line.strip().split()
-                if len(line) != self.word_dim + 1:
-                    continue
-                word2id[line[0]] = len(word2id)
-                word2vec.append(np.asarray(line[1:], dtype=np.float32))
-
-            special_emb = np.random.uniform(-1, 1, (2, self.word_dim))
-            special_emb[0] = 0  # <pad> is initialize as zero
-            word2vec = np.concatenate((special_emb, word2vec), axis=0)
-            word2vec = word2vec.astype(np.float32).reshape(-1, self.word_dim)
-            word2vec = torch.from_numpy(word2vec)
-            print("word2vec:", word2vec.shape)
+            if not os.path.exists(os.path.join(self.path_word, 'fasttext_crawl.1M.300d_word2id.json')):
+                fasttext_model = KeyedVectors.load_word2vec_format(os.path.join(self.path_word, 'crawl-300d-2M.vec'))
+                word2id = fasttext_model.vocab
+                json.dump(word2id, open(os.path.join(self.path_word,'fasttext_crawl.1M.300d_word2id.json')))
+                word2vec = np.array([fasttext_model.word_vec(k) for k in word2id.keys()])
+                np.save(os.path.join(self.path_word,'fasttext_crawl.1M.300d._mat.npy'), word2vec)
+            else:
+                word2id = json.load(open(os.path.join(self.path_word, 'fasttext_crawl.1M.300d_word2id.json')))
+                word2vec = np.load(os.path.join(self.path_word, 'fasttext_crawl.1M.300d._mat.npy'))
         
         elif 'glove' == self.embedding_name:
             word2id = json.load(open(os.path.join(self.path_word, 'glove.6B.50d_word2id.json')))
